58ee743066a555a49f9131aa5c287eff718f424d
Puffy2511 <lawrence.chen2511@gmail.com>
2026-08-21 23:15:37 +1000

    Add files via upload
    Events

---
+++ Combined Odds.py
@@ -0,0 +1,37 @@
+
+import requests
+
+API_key = "883eac145e6d5a8a5e66a856872e4c0e"
+sport = "aussierules_afl"
+region = "au"
+bookmaker = "pointsbetau"
+market = "afl_player_disposals"
+odds_format = "decimal"
+
+
+def get_events():
+
+    url = f"https://api.the-odds-api.com/v4/sports/{sport}/events"
+    params = {"api_key": API_key}
+
+    response = requests.get(url, params=params)
+
+    if response.status_code != 200:
+        print(f"Error: {response.status_code}")
+        return None
+
+    events = response.json()
+
+    if not events:
+        print("No events")
+        return None
+
+    print("AFL Games: \n")
+    for i, event in enumerate(events):
+        print(f"[{i}]: {event['home_team']} vs {event['away_team']}")
+
+    index = int(input("Choose an event: "))
+    return events[index]['id'], events[index]['home_team'], events[index]['away_team']
+
+
+
