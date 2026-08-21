e6dd76c2acb8a7cf6de4475c04655605419d8307
Puffy2511 <lawrence.chen2511@gmail.com>
2026-08-21 23:27:33 +1000

    Update Combined Odds.py

--- Combined Odds.py
+++ Combined Odds.py
@@ -5,7 +5,7 @@ API_key = "????" # <-- Go to The Odds.api and make an account, copypaste your ap
 sport = "aussierules_afl"
 region = "au"
 bookmaker = "pointsbetau"
-market = "afl_player_disposals"
+market = "player_disposals"
 odds_format = "decimal"
 
 
